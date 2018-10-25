63da4db8637d1fed1ef0a8e7995dea2fcef316ab
Heiko Klare <heiko.klare@kit.edu>
2018-10-25 17:15:19 +0200

    Add util for handling PCM primitive types repository

---
+++ bundles/pcm/tools.vitruv.domains.pcm.util/src/tools/vitruv/domains/pcm/util/PrimitiveTypesRepositoryLoader.java
@@ -0,0 +1,65 @@
+package tools.vitruv.domains.pcm.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.palladiosimulator.pcm.repository.DataType;
+import org.palladiosimulator.pcm.repository.PrimitiveDataType;
+import org.palladiosimulator.pcm.repository.Repository;
+
+public class PrimitiveTypesRepositoryLoader {
+	private PrimitiveTypesRepositoryLoader() {
+	}
+
+	private static String PRIMITIVETYPES_URI = "platform:/plugin/org.palladiosimulator.pcm.resources/defaultModels/PrimitiveTypes.repository";
+
+	private static Repository primitiveTypesRepository;
+	private static Map<String, PrimitiveDataType> primitives = new HashMap<String, PrimitiveDataType>();
+
+	/**
+	 * Retrieves a map of {@link PrimitiveDataType}s as defined in the
+	 * standard PCM resource repository.
+	 * 
+	 * @return A cached map of primitive data types.
+	 */
+	public static Map<String, PrimitiveDataType> getPrimitiveDataTypes() {
+		if (primitiveTypesRepository == null) {
+			primitiveTypesRepository = getPrimitiveTypesRepository();
+			for (DataType d : primitiveTypesRepository.getDataTypes__Repository()) {
+				if (d instanceof PrimitiveDataType) {
+					PrimitiveDataType pdt = (PrimitiveDataType) d;
+					primitives.put(pdt.getType().getName(), pdt);
+				}
+			}
+		}
+		return primitives;
+	}
+
+	private static Repository getPrimitiveTypesRepository() {
+		if (primitiveTypesRepository != null) {
+			return primitiveTypesRepository;
+		}
+
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("repository", new XMIResourceFactoryImpl());
+
+		URI uri = URI.createURI(PRIMITIVETYPES_URI);
+
+		ResourceSet resSet = new ResourceSetImpl();
+		Resource resource = resSet.getResource(uri, true);
+
+		primitiveTypesRepository = (Repository) resource.getContents().get(0);
+		return primitiveTypesRepository;
+	}
+
+	public static PrimitiveDataType getPrimitiveDataTypeString() {
+		return getPrimitiveDataTypes().get("STRING");
+	}
+
+}
