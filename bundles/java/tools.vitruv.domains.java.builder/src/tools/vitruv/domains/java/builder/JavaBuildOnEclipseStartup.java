2573e7436ebc17d7d3be1a13f3e0c7ac5ae635e0
Heiko Klare <heiko.klare@kit.edu>
2020-12-14 17:29:50 +0100

    Handle errors when initializing Java Vitruv builder

--- bundles/java/tools.vitruv.domains.java.builder/src/tools/vitruv/domains/java/builder/JavaBuildOnEclipseStartup.java
+++ bundles/java/tools.vitruv.domains.java.builder/src/tools/vitruv/domains/java/builder/JavaBuildOnEclipseStartup.java
@@ -1,17 +1,25 @@
 package tools.vitruv.domains.java.builder;
 
+import org.apache.log4j.Logger;
 import org.eclipse.ui.IStartup;
 
 import tools.vitruv.framework.ui.monitorededitor.ProjectBuildUtils;
 
 /**
- * {@link JavaBuildOnEclipseStartup} issues an incremental build of all open projects just after Eclipse
- * started. This ensures that the respective builder objects do exist (and install EMF editor
- * monitors) before the user starts editing files.
+ * {@link JavaBuildOnEclipseStartup} issues an incremental build of all open
+ * projects just after Eclipse started. This ensures that the respective builder
+ * objects do exist (and install EMF editor monitors) before the user starts
+ * editing files.
  */
 public class JavaBuildOnEclipseStartup implements IStartup {
-    @Override
-    public void earlyStartup() {
-        ProjectBuildUtils.issueIncrementalBuildForAllProjectsWithBuilder(VitruviusJavaBuilder.BUILDER_ID);
-    }
+	private static final Logger LOGGER = Logger.getLogger(JavaBuildOnEclipseStartup.class);
+
+	@Override
+	public void earlyStartup() {
+		try {
+			ProjectBuildUtils.issueIncrementalBuildForAllProjectsWithBuilder(VitruviusJavaBuilder.BUILDER_ID);
+		} catch (IllegalStateException e) {
+			LOGGER.error("Could not issue initial build for all projects", e);
+		}
+	}
 }
